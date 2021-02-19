387eb7142ff2ea6b881db84069a1f90dfc2ec7b2
Hariprasad <haridx.poojary@gmail.com>
2021-02-19 12:46:23 +0530

    Basic HTML Structure with Bootstrap & DJango Templates

--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.main_page, name="main"),
-    path("store/", views.store_page, name="store"),
+    path("", views.store_page, name="store"),
     path("cart/", views.cart_page, name="cart"),
     path("checkout/", views.checkout_page, name="checkout"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def main_page(request):
-    return render(request, "store/main.html")
-
-
 def store_page(request):
     return render(request, "store/store.html")
 
