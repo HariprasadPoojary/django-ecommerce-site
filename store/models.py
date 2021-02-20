f4307dea0b25f8a14c50ed717fda452a91e21305
Hariprasad <haridx.poojary@gmail.com>
2021-02-20 18:34:29 +0530

    Created Database app model with relations

--- store/models.py
+++ store/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField(null=False)
+    digital = models.BooleanField(null=False)
+    # image =
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    email = models.EmailField(blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=False)
+
+    def __str__(self) -> str:
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{str(self.order)}_{str(self.product)}"
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=300, null=False)
+    city = models.CharField(max_length=100, null=False)
+    state = models.CharField(max_length=100, null=False)
+    pincode = models.IntegerField(max_length=999999, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.address
